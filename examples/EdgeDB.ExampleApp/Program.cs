d597b520034a638786667501f8396a390e61e6ba
Quin Lynch <lynchquin@gmail.com>
2022-05-15 03:29:17 -0300

    remove test case from example

--- examples/EdgeDB.ExampleApp/Program.cs
+++ examples/EdgeDB.ExampleApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using EdgeDB;
 using EdgeDB.ExampleApp;
-using System.Collections.Concurrent;
-using System.Diagnostics;
 
 // Initialize our logger
 Logger.AddStream(Console.OpenStandardOutput(), StreamType.StandardOut);
@@ -13,41 +11,6 @@ var edgedb = new EdgeDBClient(new EdgeDBConfig
 {
     //Logger = Logger.GetLogger<EdgeDBClient>(LogPostfix.Debug, LogPostfix.Error, LogPostfix.Warning, LogPostfix.Info, LogPostfix.Critical)
 });
-
-//var str = await edgedb.QueryRequiredSingleAsync<string>("select \"Hello World!\"");
-
-// create 1000 tasks
-var numTasks = 1000;
-Task[] tasks = new Task[numTasks];
-ConcurrentBag<string> results = new();
-
-for (int i = 0; i != numTasks; i++)
-{
-    tasks[i] = Task.Run(async () =>
-    {
-        try
-        {
-            results.Add(await edgedb.QueryRequiredSingleAsync<string>("select \"Hello, Dotnet!\""));
-        }
-        catch (Exception x)
-        {
-            results.Add("Failed " + x);
-        }
-    });
-}
-
-Console.WriteLine("Starting 1000 query test...");
-
-Stopwatch sw = Stopwatch.StartNew();
-
-await Task.WhenAll(tasks).ConfigureAwait(false);
-
-sw.Stop();
-
-var count = results.Count(x => x == "Hello, Dotnet!");
-var failed = results.Where(x => x != "Hello, Dotnet!");
-
-Console.WriteLine($"Executed 1000 query test in {sw.ElapsedMilliseconds}ms");
 
 // Run our examples
 await IExample.ExecuteAllAsync(edgedb).ConfigureAwait(false);
