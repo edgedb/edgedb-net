093222cbb55c5442f2b96920ae105b5b65947cfe
Quin Lynch <lynchquin@gmail.com>
2022-09-15 15:41:32 -0300

    Update SnakeCaseNamingStrategy.cs

--- src/EdgeDB.Net.Driver/NamingStrategies/SnakeCaseNamingStrategy.cs
+++ src/EdgeDB.Net.Driver/NamingStrategies/SnakeCaseNamingStrategy.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace EdgeDB
 {
-    public sealed class SnakeCaseNamingStrategy : INamingStrategy
+    internal sealed class SnakeCaseNamingStrategy : INamingStrategy
     {
         public string Convert(PropertyInfo property)
             => Convert(property.Name);
