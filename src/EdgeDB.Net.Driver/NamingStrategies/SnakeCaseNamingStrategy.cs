3c1afd8d068eb0cc0d752847d839d8d059ff7d1f
Syzuna <syzuna@syzuna-programs.de>
2022-09-15 11:46:59 +0200

    Improve snake case property name conversion performance (#36)

--- src/EdgeDB.Net.Driver/NamingStrategies/SnakeCaseNamingStrategy.cs
+++ src/EdgeDB.Net.Driver/NamingStrategies/SnakeCaseNamingStrategy.cs
@@ -14,6 +14,38 @@ namespace EdgeDB
             => Convert(property.Name);
 
         public string Convert(string name)
-            => Regex.Replace(name, "(?<!^)[A-Z]", x => $"_{x.Value}").ToLower();
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return name;
+            }
+
+            var upperCaseLength = name.Where((c, i) => c is >= 'A' and <= 'Z' && i != 0).Count();
+
+            if (upperCaseLength == 0)
+                return name;
+
+            var bufferSize = name.Length + upperCaseLength;
+            Span<char> buffer = stackalloc char[bufferSize];
+            var bufferPosition = 0;
+            var namePosition = 0;
+            while (bufferPosition < buffer.Length)
+            {
+                if (namePosition > 0 && name[namePosition] >= 'A' && name[namePosition] <= 'Z')
+                {
+                    buffer[bufferPosition] = '_';
+                    buffer[bufferPosition + 1] = name[namePosition];
+                    bufferPosition += 2;
+                    namePosition++;
+                    continue;
+                }
+                buffer[bufferPosition] = name[namePosition];
+                bufferPosition++;
+                namePosition++;
+            }
+
+            return new string(buffer).ToLower();
+        }
+            
     }
 }
