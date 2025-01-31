badea9e85d73573076ade7e8687c35da070d6c14
dnwpark <dnwpark@protonmail.com>
2025-01-31 16:52:41 -0500

    Rename project EdgeDB.DotnetTool to Gel.DotnetTool.

--- src/Gel.Net.Driver/AssemblyInfo.cs
+++ src/Gel.Net.Driver/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("EdgeDB.Net.QueryBuilder")]
 [assembly: InternalsVisibleTo("EdgeDB.Runtime")]
 [assembly: InternalsVisibleTo("EdgeDB.ExampleApp")]
-[assembly: InternalsVisibleTo("EdgeDB.DotnetTool")]
+[assembly: InternalsVisibleTo("Gel.DotnetTool")]
 [assembly: InternalsVisibleTo("Gel.Tests.Unit")]
 [assembly: InternalsVisibleTo("Gel.Tests.Integration")]
 [assembly: InternalsVisibleTo("Gel.Tests.Benchmarks")]
