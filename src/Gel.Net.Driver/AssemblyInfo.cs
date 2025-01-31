89f5e0a1cabca9e95ec12b33bcd2ed96df9e4095
dnwpark <dnwpark@protonmail.com>
2025-01-31 16:43:24 -0500

    Rename project EdgeDB.Tests.Benchmarks to Gel.Tests.Benchmarks.

--- src/Gel.Net.Driver/AssemblyInfo.cs
+++ src/Gel.Net.Driver/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("EdgeDB.DotnetTool")]
 [assembly: InternalsVisibleTo("EdgeDB.Tests.Unit")]
 [assembly: InternalsVisibleTo("EdgeDB.Tests.Integration")]
-[assembly: InternalsVisibleTo("EdgeDB.Tests.Benchmarks")]
+[assembly: InternalsVisibleTo("Gel.Tests.Benchmarks")]
 [assembly: InternalsVisibleTo("EdgeDB.BinaryDebugger")]
 [assembly: InternalsVisibleTo("EdgeDB.Serializer.Experiments")]
